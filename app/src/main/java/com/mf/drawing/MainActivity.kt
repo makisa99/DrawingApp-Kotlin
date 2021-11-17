034cff8ea4ee41f61187af2162129084309fbbf2
makisa99 <72605856+makisa99@users.noreply.github.com>
2021-11-17 15:17:50 +0100

    colors done
    TODO: every color changes everytime i select a new one

--- app/src/main/java/com/mf/drawing/MainActivity.kt
+++ app/src/main/java/com/mf/drawing/MainActivity.kt
@@ -3,13 +3,19 @@ package com.mf.drawing
 import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.ImageButton
+import android.widget.LinearLayout
+import android.widget.Toast
+import androidx.core.content.ContextCompat
+import androidx.core.view.get
 import kotlinx.android.synthetic.main.dialog_brush_size.*
 
 class MainActivity : AppCompatActivity() {
 
     private var drawingView: DrawingView? = null
+    private var mImageButtonCurrentPaint: ImageButton? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,8 +24,16 @@ class MainActivity : AppCompatActivity() {
         drawingView = findViewById(R.id.drawing_view)
         drawingView?.setSizeForBrush(20.toFloat())
 
+        val linearLayoutPaintColors = findViewById<LinearLayout>(R.id.ll_paint_colors)
+
+        mImageButtonCurrentPaint = linearLayoutPaintColors[1] as ImageButton
+        mImageButtonCurrentPaint!!.setImageDrawable(
+            ContextCompat.getDrawable(this, R.drawable.pallete_pressed)
+
+        )
+
         val id_brush: Button = findViewById(R.id.id_brush)
-        id_brush.setOnClickListener{
+        id_brush.setOnClickListener {
             showBrushSizeChooserDialog()
         }
 
@@ -48,5 +62,21 @@ class MainActivity : AppCompatActivity() {
         brushDialog.show()
     }
 
+    fun paintClicked(view: View) {
+        if(view !== mImageButtonCurrentPaint){
+            val imageButton = view as ImageButton
+            val colorTag = imageButton.tag.toString()
+            drawingView?.setColor(colorTag)
 
+            imageButton.setImageDrawable(
+                ContextCompat.getDrawable(this, R.drawable.pallete_pressed)
+            )
+
+            mImageButtonCurrentPaint?.setImageDrawable(
+                ContextCompat.getDrawable(this, R.drawable.pallete_normal)
+            )
+
+            mImageButtonCurrentPaint = view
+        }
+    }
 }
